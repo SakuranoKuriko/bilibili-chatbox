38369d72d28582dc9ee9ba1f91dcb15df0d74af2
SakuranoKuriko <Mao.Liz.Baka@Gmail.com>
2019-03-09 23:04:17 +0800

    v1.0

--- index.php
+++ index.php
@@ -215,7 +215,7 @@
             <input type="text" v-model="conf.timeformat" />
           </div>
           <button key="applyconfbtn" @click="applyconf">OK</button>
-          <a id="about" href="https://github.com/SakuranoKuriko/bilibili-chatbox">Source code</a>
+          <a id="about" target="_blank" href="https://github.com/SakuranoKuriko/bilibili-chatbox">Source code</a>
         </div>
       </div>
     </transition>
